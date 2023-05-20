656649691ebf5e21aed8fc945aca03bf72e3afdf
raundev <lbil7641@gmail.com>
2023-05-20 16:28:07 +0900

    Initial commit

---
+++ src/test/java/com/eorl/EorlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eorl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EorlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
