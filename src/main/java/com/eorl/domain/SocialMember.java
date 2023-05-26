a1964533e51f48c5951dbdf99d00c114efd25d41
baeyuyeon <dbdus983@naver.com>
2023-05-26 23:17:27 +0900

    Member, SocialMember domain, jpa를 이용한 테이블 생성

---
+++ src/main/java/com/eorl/domain/SocialMember.java
@@ -0,0 +1,42 @@
+package com.eorl.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name = "SocialMember")
+@Getter
+@ToString
+@NoArgsConstructor
+public class SocialMember {
+
+    @Id
+    @Column(name = "member_id")
+    private int memberId;
+
+    @Id
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
+
+    @Column(columnDefinition="DATETIME")
+    private LocalDateTime registrationDatetime;
+
+    @Column(columnDefinition="DATETIME")
+    private LocalDateTime modificationDatetime;
+
+}
+enum SocialType{
+    GOOGLE,
+    APPLE,
+    NAVER
+}
