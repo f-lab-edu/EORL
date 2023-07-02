1b37c2e9d3ff72653680939e4a0b9abbb73c88d6
raundev <lbil7641@gmail.com>
2023-07-02 23:49:52 +0900

    Menu 및 Store 도메인 변경(EnumData 추가)

--- src/main/java/com/eorl/domain/common/EnumData.java
+++ src/main/java/com/eorl/domain/common/EnumData.java
@@ -1,18 +1,8 @@
 package com.eorl.domain.common;
 
-import com.eorl.domain.store.StoreStatus;
-import java.util.Arrays;
-import java.util.NoSuchElementException;
-
-public interface EnumData<T> {
+public interface EnumData {
 
     String getCode();
     String getTitle();
 
-    /*static EnumData getInstance(String code) {
-        return Arrays.stream(StoreStatus.values())
-                .filter(s -> s.getCode().equals(code))
-                .findAny()
-                .orElseThrow(()-> new NoSuchElementException());
-    };*/
 }
