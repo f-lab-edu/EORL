2822acc7c42ff7fe85a1f9b1492fb692ecb19f93
baeyuyeon <dbdus983@naver.com>
2023-07-11 14:47:32 +0900

    [EORL-11] pr 리뷰건 수정

--- src/test/java/com/eorl/service/notification/NotificationServiceImplTest.java
+++ src/test/java/com/eorl/service/notification/NotificationServiceImplTest.java
@@ -1,17 +1,14 @@
 package com.eorl.service.notification;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import com.eorl.domain.notification.Notification;
-import com.eorl.domain.notification.NotificationSaveForm;
 import com.eorl.domain.member.member.Member;
 import com.eorl.domain.member.member.MemberSaveForm;
+import com.eorl.domain.notification.Notification;
+import com.eorl.domain.notification.NotificationSaveForm;
 import com.eorl.domain.notification.NotificationStatus;
 import com.eorl.service.member.MemberService;
-import java.awt.print.PageFormat;
-import java.awt.print.Pageable;
-import java.awt.print.Printable;
 import java.util.NoSuchElementException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -50,27 +47,35 @@ class NotificationServiceImplTest {
     @DisplayName("알람등록 테스트")
     void registerAlarm() {
         //given
-        NotificationSaveForm notificationSaveForm = new NotificationSaveForm(member.getMemberId(), member.getMemberId()
+        NotificationSaveForm notificationSaveForm = new NotificationSaveForm(member.getMemberId(),
+                member.getMemberId()
                 , "알람메시지 테스트", "NAVER", "PENDING");
-        Notification notification = new Notification(notificationSaveForm.getSendMemberId(), notificationSaveForm.getReceiveMemberId()
-                , notificationSaveForm.getNotificationMsg(), notificationSaveForm.getNotificationKind()
+        Notification notification = new Notification(notificationSaveForm.getSendMemberId(),
+                notificationSaveForm.getReceiveMemberId()
+                , notificationSaveForm.getNotificationMsg(),
+                notificationSaveForm.getNotificationKind()
                 , notificationSaveForm.getNotificationStatus());
         //when
         notificationService.registerNotification(notification);
 
         //then
-        Notification findNotification = notificationService.findByNotificationId(notification.getNotificationId());
-        assertThat(notification.getNotificationId()).isEqualTo(findNotification.getNotificationId());
+        Notification findNotification = notificationService.findByNotificationId(
+                notification.getNotificationId());
+        assertThat(notification.getNotificationId()).isEqualTo(
+                findNotification.getNotificationId());
     }
 
     @Test
     @DisplayName("존재하지 않는 멤버가 알람등록하기")
     void registerNoneUserAlarm() {
         //given
-        NotificationSaveForm notificationSaveForm = new NotificationSaveForm(99999999L, member.getMemberId()
+        NotificationSaveForm notificationSaveForm = new NotificationSaveForm(99999999L,
+                member.getMemberId()
                 , "알람메시지 테스트", "NAVER", "PENDING");
-        Notification notification = new Notification(notificationSaveForm.getSendMemberId(), notificationSaveForm.getReceiveMemberId()
-                , notificationSaveForm.getNotificationMsg(), notificationSaveForm.getNotificationKind()
+        Notification notification = new Notification(notificationSaveForm.getSendMemberId(),
+                notificationSaveForm.getReceiveMemberId()
+                , notificationSaveForm.getNotificationMsg(),
+                notificationSaveForm.getNotificationKind()
                 , notificationSaveForm.getNotificationStatus());
 
         //when
@@ -78,42 +83,50 @@ class NotificationServiceImplTest {
         assertThrows(NoSuchElementException.class,
                 () -> notificationService.registerNotification(notification));
     }
+
     @Test
     @DisplayName("paging 처리 테스트")
-    void findAllOrderByNotificationId(){
+    void findAllOrderByNotificationId() {
         //given
-        for(int i=0; i<3; i++){
-            NotificationSaveForm notificationSaveForm = new NotificationSaveForm(member.getMemberId(), member.getMemberId()
+        for (int i = 0; i < 3; i++) {
+            NotificationSaveForm notificationSaveForm = new NotificationSaveForm(
+                    member.getMemberId(), member.getMemberId()
                     , "알람메시지 테스트", "NAVER", "PENDING");
-            Notification notification = new Notification(notificationSaveForm.getSendMemberId(), notificationSaveForm.getReceiveMemberId()
-                    , notificationSaveForm.getNotificationMsg(), notificationSaveForm.getNotificationKind()
+            Notification notification = new Notification(notificationSaveForm.getSendMemberId(),
+                    notificationSaveForm.getReceiveMemberId()
+                    , notificationSaveForm.getNotificationMsg(),
+                    notificationSaveForm.getNotificationKind()
                     , notificationSaveForm.getNotificationStatus());
             notificationService.registerNotification(notification);
         }
-        PageRequest pageable = PageRequest.of(0,10);
+        PageRequest pageable = PageRequest.of(0, 10);
         //when
-        Page<Notification> allOrderByNotificationId = notificationService.findAllOrderByNotificationId(pageable);
+        Page<Notification> allOrderByNotificationId = notificationService.findAllOrderByNotificationId(
+                pageable);
 
         assertThat(allOrderByNotificationId.getTotalElements()).isEqualTo(3);
 
     }
+
     @Test
     @DisplayName("상태 전송완료로 업데이트하기.")
-    void updateNotificationStatus(){
+    void updateNotificationStatus() {
         //given
-        NotificationSaveForm notificationSaveForm = new NotificationSaveForm(member.getMemberId(), member.getMemberId()
+        NotificationSaveForm notificationSaveForm = new NotificationSaveForm(member.getMemberId(),
+                member.getMemberId()
                 , "알람메시지 테스트", "NAVER", "PENDING");
-        Notification notification = new Notification(notificationSaveForm.getSendMemberId(), notificationSaveForm.getReceiveMemberId()
-                , notificationSaveForm.getNotificationMsg(), notificationSaveForm.getNotificationKind()
+        Notification notification = new Notification(notificationSaveForm.getSendMemberId(),
+                notificationSaveForm.getReceiveMemberId()
+                , notificationSaveForm.getNotificationMsg(),
+                notificationSaveForm.getNotificationKind()
                 , notificationSaveForm.getNotificationStatus());
         notificationService.registerNotification(notification);
-
         //when
         notificationService.updateNotificationStatus(notification.getNotificationId(),
                 NotificationStatus.COMPLETE);
-
         //then
-        Notification findNotification = notificationService.findByNotificationId(notification.getNotificationId());
+        Notification findNotification = notificationService.findByNotificationId(
+                notification.getNotificationId());
         assertThat(findNotification.getNotificationStatus()).isEqualTo(NotificationStatus.COMPLETE);
     }
 
